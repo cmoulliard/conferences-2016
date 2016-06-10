c39d6db3301da7d404fb0818fc4e7e295adb210e
Charles Moulliard <cmoulliard@apache.org>
2016-06-10 09:48:44 +0200

    Reformat code

--- generate_slideshow.sh
+++ generate_slideshow.sh
@@ -2,7 +2,7 @@
 
 # Generate Slideshow using revealjs-redhat template
 hyla generate -c slideshow/conference.yaml
-compass compile --fonts-dir 'fonts' --css-dir 'slideshow/generated/revealjs-redhat/lib/css' --sass-dir 'slideshow/sass' -e development --output-style=expanded --force
+#compass compile --fonts-dir 'fonts' --css-dir 'slideshow/generated/revealjs-redhat/lib/css' --sass-dir 'slideshow/sass' -e development --output-style=expanded --force
 
 # Generate Slideshow using revealjs template (temaplet & css should be reviewed - NOT USED)
 # compass compile --fonts-dir 'fonts' --css-dir 'slideshow/generated/revealjs/css/theme' --sass-dir 'slideshow/sass' -e development --output-style=expanded --force
