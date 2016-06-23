ed7829c9188250a6ba24b5b2f59f7595a4b6f0e2
Charles Moulliard <cmoulliard@apache.org>
2016-06-23 15:01:22 +0200

    Update script and test done with keycloak deployed on OpenShift

--- scripts/getToken.sh
+++ scripts/getToken.sh
@@ -1,10 +1,16 @@
 #!/usr/bin/env bash
 
+# Locally
+
 #host=http://localhost:8080
 #host=https://192.168.1.80:8443
-host=https://localhost:8443
+#host=https:localhost//:8443
 
-auth_result=$(curl -k -X POST $host/auth/realms/demo/protocol/openid-connect/token -d grant_type=password -d username=admin -d password=admin -d client_id=demo)
+# Host deployed in Openshift
+host=https://keycloak.vagrant.f8
+
+#auth_result=$(http --verify=no --verbose -f $HOST/auth/realms/demo/protocol/openid-connect/token username=admin password=admin grant_type=password client_id=demo)
+auth_result=$(curl -sk -X POST $HOST/auth/realms/demo/protocol/openid-connect/token -d grant_type=password -d username=admin -d password=admin -d grant_type=password -d client_id=demo)
 access_token=$(echo -e "$auth_result" | awk -F"," '{print $1}' | awk -F":" '{print $2}' | sed s/\"//g | tr -d ' ')
 
 echo $access_token
